e11937f2082899e8d3396ff20fc9ab37c06d0fb4
suminda123 <suminda123@gmail.com>
2016-07-20 23:52:14 +1200

    work with streams

---
+++ stream.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+const highland = require("highland");
+highland(fs.createReadStream("customer.csv","utf8"))
+    .split()
+    .map(line => line.split(','))
+    .map(parts => ({
+        fullname : parts[0] + ' ' + parts[1],
+        num1 : parts[2]
+    }))
+    .each(x => console.log("each: " , x));
